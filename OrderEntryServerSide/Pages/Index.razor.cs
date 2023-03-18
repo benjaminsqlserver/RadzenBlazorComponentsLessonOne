f9778b7d1177d88c761947c66ddfec017c464828
Benjamin Fadina <benjamin.fadina@africaprudential.com>
2023-03-18 05:32:17 +0100

    First Commit

---
+++ OrderEntryServerSide/Pages/Index.razor.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Components;
+using Radzen;
+
+namespace OrderEntryServerSide.Pages
+{
+    public partial class Index
+    {
+        [Inject]
+
+        protected NotificationService NotificationService { get; set; }
+
+
+        private void ButtonClicked()
+        {
+            //display a message saying welcome to RADZEN Blazor Components
+            NotificationService.Notify(NotificationSeverity.Success, "Welcome", "Welcome To Radzen Blazor Components!", 10000);
+        }
+    }
+}
